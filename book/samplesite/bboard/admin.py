a95653c3cb93ed906b303079cb15f70bf82c639e
Denny <keplerde@gmail.com>
2021-12-14 13:49:17 +0100

    Model + Zusammenhang zwischen Modell

--- book/samplesite/bboard/admin.py
+++ book/samplesite/bboard/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from .models import Rubric
+admin.site.register(Rubric)
 
 from .models import Bb
 
--- book/samplesite/bboard/models.py
+++ book/samplesite/bboard/models.py
@@ -5,8 +5,18 @@ class Bb(models.Model):
     content = models.TextField(null=True, blank=True, verbose_name='Description')
     price = models.FloatField(null=True, blank=True, verbose_name='Price')
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Published')
+    rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Kategorie')
 
     class Meta:
         verbose_name_plural = 'Anzeigen'
         verbose_name = 'Material'
         ordering = ['-published']
+
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, db_index=True, verbose_name='Name')
+
+    class Meta:
+        verbose_name_plural = 'Kategorien'
+        verbose_name = 'Kategorie'
+        ordering = ['name']
